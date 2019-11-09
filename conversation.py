332522446a75a18ff07a236ca117c0ac2039b2bf
clarkandrew10 <51393498+clarkandrew10@users.noreply.github.com>
2019-11-09 16:25:52 -0500

    Fixing token issue

--- conversation.py
+++ conversation.py
@@ -1,7 +1,5 @@
 from twilio.rest import Client
 
-account_sid = 'AC3b9bda4723be316e90ed657a40130d5b'
-auth_token = 'c826e7ec7ba59cea0025b8c1aca0621d'
 client = Client(account_sid, auth_token)
 
 conversation = client.conversations \
