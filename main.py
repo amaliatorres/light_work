49d2d93bdc3931b346c862846b5a66d86f990a86
clarkandrew10 <51393498+clarkandrew10@users.noreply.github.com>
2019-11-10 00:01:59 -0500

    12:00Am

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ jinja_enviroment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
-
-
 class IntroPage(webapp2.RequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
         template = jinja_enviroment.get_template("/templates/index.html")
+
+        self.response.write(template.render())
+
+class Search(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        template = jinja_enviroment.get_template("/templates/search.html")
 
         self.response.write(template.render())
 class Signin(webapp2.RequestHandler):
@@ -26,9 +32,8 @@ class Signin(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/', IntroPage),
-    ('/sign_in',Signin)
-
-
+    ('/sign_in',Signin),
+    ('/search',Search)
 
 
 ], debug=True)
